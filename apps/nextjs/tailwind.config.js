df0dc3875e2920a65569c0eb6a9b2d73d2b217c4
jimmynXD <87709880+jimmynXD@users.noreply.github.com>
2022-08-24 18:14:19 -0400

    feat: nextjs (#1)

---
+++ apps/nextjs/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require("@labxd/gustxd/tailwind")],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/@labxd/gustxd/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+};
