7c505436210d8ee0ff68a528d0e6294f45bd3a01
walleXD <6826840+walleXD@users.noreply.github.com>
2022-09-17 12:07:02 -0400

    feat(surveyxd): survey creation on FE & BE (#25)

--- apps/surveyxd/src/modules/survey/api/routes.ts
+++ apps/surveyxd/src/modules/survey/api/routes.ts
@@ -1,21 +1,9 @@
 import { z } from "zod"
 
-import { SurveyPublishStatus } from "@/prisma"
+import { SurveyPublishStatus, QuestionType } from "@/prisma"
 import { createRouter } from "@/trpc/api"
 
 export const surveyRoutes = createRouter()
-  .mutation("createSurvey", {
-    input: z.object({
-      title: z.string().optional(),
-    }),
-    resolve: async ({ ctx, input }) => {
-      return ctx.prisma.survey.create({
-        data: {
-          title: input.title,
-        },
-      })
-    },
-  })
   .mutation("updateSurvey", {
     input: z.object({
       surveyId: z.string(),
@@ -34,6 +22,53 @@ export const surveyRoutes = createRouter()
       })
     },
   })
+  .mutation("createSurvey", {
+    input: z.object({
+      title: z.string(),
+      questions: z
+        .object({
+          required: z.boolean().optional(),
+          title: z.string(),
+          type: z.nativeEnum(QuestionType),
+          options: z
+            .object({
+              value: z.string(),
+            })
+            .array(),
+        })
+        .array(),
+    }),
+    resolve: async ({ input, ctx }) => {
+      console.log(`Survey created with title ${input.title}`)
+
+      return await ctx.prisma.survey.create({
+        data: {
+          title: input.title,
+          publishStatus: SurveyPublishStatus.PUBLISHED,
+          questions: {
+            create: input.questions.map((q) => ({
+              title: q.title,
+              questionType: q.type,
+              options: {
+                create: q.options.map((opt, i) => ({
+                  numericValue: i,
+                  textValue: opt.value,
+                  questionType: q.type,
+                })),
+              },
+            })),
+          },
+        },
+        include: {
+          questions: {
+            include: {
+              options: true,
+            },
+          },
+        },
+      })
+    },
+  })
   .query("getSurvey", {
     input: z.object({
       surveyId: z.string(),
@@ -41,6 +76,16 @@ export const surveyRoutes = createRouter()
     resolve: async ({ ctx, input }) => {
       return ctx.prisma.survey.findUnique({
         where: { id: input.surveyId },
+        select: {
+          questions: {
+            select: {
+              id: true,
+              title: true,
+              createdAt: true,
+              options: true,
+            },
+          },
+        },
       })
     },
   })
