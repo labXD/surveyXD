bb68de869bcad3231589c857d8eaa2b32dd6e72b
Jimmy N <jamesfrontend@gmail.com>
2022-09-19 11:40:57 -0400

    fix: typecheck

--- apps/surveyxd/src/modules/survey/web/containers/sandDanceWrapper.tsx
+++ apps/surveyxd/src/modules/survey/web/containers/sandDanceWrapper.tsx
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic"
 
+// This is the component that will not be rendered on the server
 export const XDSandDanceNoSSR = dynamic(
   () => import("@/survey/web/components/Explorer"),
   {
