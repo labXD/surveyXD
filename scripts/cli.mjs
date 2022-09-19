38ea9dd0fcde15613d1253d71974973b89d0afcb
Walee Ahmed <6826840+walleXD@users.noreply.github.com>
2022-09-19 19:58:04 -0400

    ci: set app env with cli

--- scripts/cli.mjs
+++ scripts/cli.mjs
@@ -2,10 +2,14 @@
 
 import yargs from "yargs/yargs"
 
+import { command as ciCommand } from "./ci.mjs"
 import { command as deployCommand } from "./deploy.mjs"
 
 const cli = async () =>
-  yargs(process.argv.slice(2)).command(deployCommand).demandCommand().help()
-    .argv
+  yargs(process.argv.slice(2))
+    .command(deployCommand)
+    .command(ciCommand)
+    .demandCommand()
+    .help().argv
 
 cli()
